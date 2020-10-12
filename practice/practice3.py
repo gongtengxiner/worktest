0af35b813f7349f4e98cf39a31b95ad69817d2c0
gongtengxiner <1097262505@qq.com>
2020-10-12 23:09:34 +0800

    grc 2

---
+++ practice/practice3.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Practice3
+# GNU Radio version: 3.7.14.0
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
+from optparse import OptionParser
+import sip
+import sys
+import time
+from gnuradio import qtgui
+
+
+class practice3(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Practice3")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Practice3")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "practice3")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 250000
+        self.gain = gain = 40
+        self.freq = freq = 88.5e6
+        self.audio_rate = audio_rate = 48000
+        self.audio_interp = audio_interp = 4
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._gain_range = Range(0, 74, 1, 40, 200)
+        self._gain_win = RangeWidget(self._gain_range, self.set_gain, 'gain', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._gain_win)
+        self._freq_range = Range(70e6, 6e9, 1000, 88.5e6, 200)
+        self._freq_win = RangeWidget(self._freq_range, self.set_freq, 'freq', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._freq_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("serial=31DD6A3", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(freq, 0)
+        self.uhd_usrp_source_0.set_gain(gain, 0)
+        self.uhd_usrp_source_0.set_antenna('TX/RX', 0)
+        self.uhd_usrp_source_0.set_bandwidth(samp_rate, 0)
+        self.uhd_usrp_source_0.set_auto_dc_offset(True, 0)
+        self.uhd_usrp_source_0.set_auto_iq_balance(True, 0)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=audio_rate*audio_interp,
+                decimation=int(samp_rate),
+                taps=None,
+                fractional_bw=None,
+        )
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	freq, #fc
+        	samp_rate, #bw
+        	"QT GUI Sink", #name
+        	True, #plotfreq
+        	False, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+
+
+        self.audio_sink_0 = audio.sink(audio_rate, '', False)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=audio_rate*audio_interp,
+        	audio_decimation=audio_interp,
+        )
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.rational_resampler_xxx_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "practice3")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.uhd_usrp_source_0.set_bandwidth(self.samp_rate, 0)
+        self.qtgui_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.uhd_usrp_source_0.set_gain(self.gain, 0)
+
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.uhd_usrp_source_0.set_center_freq(self.freq, 0)
+        self.qtgui_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+
+    def get_audio_rate(self):
+        return self.audio_rate
+
+    def set_audio_rate(self, audio_rate):
+        self.audio_rate = audio_rate
+
+    def get_audio_interp(self):
+        return self.audio_interp
+
+    def set_audio_interp(self, audio_interp):
+        self.audio_interp = audio_interp
+
+
+def main(top_block_cls=practice3, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
